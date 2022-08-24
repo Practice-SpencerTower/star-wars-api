de97c3355c311a46f8d0b491bf9a4012c8b2be45
SpencerTower <spencerjtower1@gmail.com>
2022-08-24 13:28:24 -0700

    refactors to query firebase, adds post request to post movies to firebase

--- src/App.js
+++ src/App.js
@@ -12,23 +12,28 @@ function App() {
         setIsloading(true);
         setError(null);
         try {
-            const response = await fetch('https://swapi.dev/api/films/');
+            // firebase API
+            const response = await fetch(
+                'https://react-http-9f7be-default-rtdb.firebaseio.com/movies.json'
+            );
             if (!response.ok) {
                 throw new Error('Something went wrong!');
             }
             const data = await response.json();
 
-            const transformedMovies = data.results.map((movieData) => {
-                return {
-                    id: movieData.episode_id,
-                    title: movieData.title,
-                    openingText: movieData.opening_crawl,
-                    releaseDate: movieData.release_date,
-                };
-            });
-            setMovies(transformedMovies);
+            const loadedMovies = [];
+
+            for (const key in data) {
+                loadedMovies.push({
+                    id: key,
+                    title: data[key].title,
+                    openingText: data[key].openingText,
+                    releaseDate: data[key].releaseDate,
+                });
+            }
+            setMovies(loadedMovies);
         } catch (e) {
-            console.log(e);
+            setError(e.message);
         }
         setIsloading(false);
     }, []);
@@ -37,8 +42,21 @@ function App() {
         fetchMoviesHandler();
     }, [fetchMoviesHandler]);
 
-    function addMovieHandler(movie) {
-        console.log(movie);
+    async function addMovieHandler(movie) {
+        // fetch can also be used to send data
+        const response = await fetch(
+            'https://react-http-9f7be-default-rtdb.firebaseio.com/movies.json',
+            {
+                method: 'POST',
+                body: JSON.stringify(movie),
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            }
+        );
+        const data = await response.json();
+
+        console.log('POST Response Data: ', data);
     }
 
     let content = <p>Found no movies.</p>;
